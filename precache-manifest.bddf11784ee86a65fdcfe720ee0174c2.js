587cf945aed5b85075486be92767ce87ad2dce6a
Rayyan <ryankassim60@gmail.com>
2024-06-05 11:36:58 +0530

    Updates

---
+++ precache-manifest.bddf11784ee86a65fdcfe720ee0174c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374ad58ae8aa922a7cef9f339288af17",
+    "url": "/roter/index.html"
+  },
+  {
+    "revision": "2686022c84d725b6d7bc",
+    "url": "/roter/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "cff80fcf135972e1440c",
+    "url": "/roter/static/js/2.3ad00c15.chunk.js"
+  },
+  {
+    "revision": "97b5938ef990e58844f0cff8514cc372",
+    "url": "/roter/static/js/2.3ad00c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c16f0d515b5c4ee12b5",
+    "url": "/roter/static/js/3.a5adbd85.chunk.js"
+  },
+  {
+    "revision": "2686022c84d725b6d7bc",
+    "url": "/roter/static/js/main.d348eb89.chunk.js"
+  },
+  {
+    "revision": "3b3df34eec8bb31a3a6a",
+    "url": "/roter/static/js/runtime-main.5a1d338c.js"
+  }
+]);
